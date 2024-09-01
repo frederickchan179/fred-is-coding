4e905956e4e1c75e026f5a82154a2db395719b58
Frederick Chan <hoa.tran@solislab.com>
2024-09-01 20:18:36 +0700

    :heavy_plus_sign: Integrate icon, dayjs module to Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,22 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@vueuse/nuxt', '@pinia/nuxt', '@nuxtjs/color-mode'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxt/icon',
+    'dayjs-nuxt'
+  ],
   colorMode: {
     classSuffix: ''
+  },
+  dayjs: {
+    locales: ['en'],
+    plugins: ['relativeTime', 'utc', 'timezone'],
+    defaultLocale: 'en',
+    defaultTimezone: 'America/New_York'
   }
 })
