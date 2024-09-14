5fac3ef162261b2a239e4ace74c9e24620367c26
Frederick Chan <hoa.tran@solislab.com>
2024-09-14 17:44:07 +0700

    :art: Small code improvement in advice-generator-app

--- pages/shots/advice-generator-app.vue
+++ pages/shots/advice-generator-app.vue
@@ -14,7 +14,6 @@ type Advice = {
 
 const {
   data: advice,
-  isLoading,
   isFetching,
   refetch
 } = useQuery<Advice>({
@@ -31,9 +30,7 @@ const refreshAdvice = () => {
 </script>
 
 <template>
-  <section
-    class="root flex h-screen w-screen items-center justify-center overflow-hidden bg-[var(--color-bg)] text-center text-[var(--color-text)] transition-all"
-  >
+  <section class="root">
     <h1 class="sr-only">
       {{ title }}
     </h1>
@@ -41,21 +38,16 @@ const refreshAdvice = () => {
     <div
       class="relative m-3 flex w-full max-w-[25rem] flex-col items-center justify-center gap-5 rounded-lg bg-[var(--color-card-bg)] px-6 py-8 shadow-2xl transition-all"
     >
-      <Icon v-if="isLoading" name="eos-icons:three-dots-loading" class="text-[4rem]" />
+      <Icon v-if="isFetching" name="eos-icons:three-dots-loading" class="text-[4rem]" />
       <template v-else>
         <h2 class="text-[0.65rem] uppercase tracking-[0.2em] text-[var(--color-highlight)] transition-all">
           Advice #{{ advice?.slip.id }}
         </h2>
         <p class="text-lg">{{ advice?.slip.advice }}</p>
-        <span class="divider mb-6 inline-flex w-full items-center gap-2">
+        <span class="divider">
           <Icon name="hugeicons:quote-down" class="text-lg" />
         </span>
-        <button
-          aria-label="Get new advice"
-          :disabled="isFetching"
-          class="refresh-button absolute bottom-0 left-1/2 inline-flex -translate-x-1/2 translate-y-1/2 appearance-none items-center justify-center rounded-full bg-[var(--color-highlight)] p-3 text-center text-[var(--color-bg)] transition-all disabled:cursor-progress disabled:grayscale"
-          @click="refreshAdvice"
-        >
+        <button aria-label="Get new advice" :disabled="isFetching" class="refresh-button" @click="refreshAdvice">
           <Icon name="game-icons:perspective-dice-six-faces-random" class="text-3xl" />
         </button>
       </template>
@@ -65,7 +57,7 @@ const refreshAdvice = () => {
 
 <style lang="postcss" scoped>
 .root {
-  @apply font-['Manrope'];
+  @apply flex h-screen w-screen items-center justify-center overflow-hidden bg-[var(--color-bg)] text-center font-['Manrope'] text-[var(--color-text)] transition-all;
 
   --color-text: hsl(193, 38%, 86%);
   --color-highlight: hsl(150, 100%, 66%);
@@ -83,6 +75,8 @@ const refreshAdvice = () => {
 }
 
 .divider {
+  @apply mb-6 inline-flex w-full items-center gap-2;
+
   &::before,
   &::after {
     @apply h-[1px] w-full flex-1 bg-[var(--color-divider)] transition-all content-[''];
@@ -90,6 +84,8 @@ const refreshAdvice = () => {
 }
 
 .refresh-button {
+  @apply absolute bottom-0 left-1/2 inline-flex -translate-x-1/2 translate-y-1/2 appearance-none items-center justify-center rounded-full bg-[var(--color-highlight)] p-3 text-center text-[var(--color-bg)] transition-all disabled:cursor-progress disabled:grayscale;
+
   --shadow-size: 5px;
 
   box-shadow: 0px 0px var(--shadow-size) 0px var(--color-highlight);
