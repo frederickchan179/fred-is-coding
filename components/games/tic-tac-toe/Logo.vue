2c350eb77c01fd725aceef7e40d26fc29dcfcbcc
Frederick Chan <hoa.tran@solislab.com>
2024-09-21 12:36:28 +0700

    :construction: Prepare some icon components

---
+++ components/games/tic-tac-toe/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center gap-3">
+    <GamesTicTacToeIconX class="text-[var(--color-primary)]" />
+    <GamesTicTacToeIconO class="text-[var(--color-secondary)]" />
+  </div>
+</template>
