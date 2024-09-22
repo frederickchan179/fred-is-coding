da34a863cd2064e859f997c6d0cd0c0a39308152
Frederick Chan <hoa.tran@solislab.com>
2024-09-22 11:54:43 +0700

    :construction: WIP on building game board with responsive

---
+++ components/games/tic-tac-toe/Board.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const store = useGameTicTacToeStore()
+const { makeMove } = store
+const { board, boardSize } = storeToRefs(store)
+</script>
+
+<template>
+  <div
+    class="mx-auto grid w-max max-w-full flex-1 gap-4 overflow-scroll p-4"
+    :style="{
+      gridTemplateColumns: `repeat(${boardSize}, minmax(60px, ${26 / 3}rem))`
+    }"
+  >
+    <template v-for="(row, rowIndex) in board" :key="rowIndex">
+      <template v-for="(col, colIndex) in row" :key="colIndex">
+        <GamesTicTacToeShadow3dBox
+          interactive
+          class="flex aspect-square items-center justify-center rounded-xl bg-[var(--color-cell-bg)]"
+          @click.prevent="makeMove(rowIndex, colIndex)"
+        >
+          <GamesTicTacToeIconMarkX v-if="col === 'X'" class="block !h-1/2 !w-1/2 text-[var(--color-primary)]" />
+          <GamesTicTacToeIconMarkO v-else-if="col === 'O'" class="block !h-1/2 !w-1/2 text-[var(--color-secondary)]" />
+          <span v-else></span>
+        </GamesTicTacToeShadow3dBox>
+      </template>
+    </template>
+  </div>
+</template>
+
+<style></style>
--- pages/games/tic-tac-toe/in-game.vue
+++ pages/games/tic-tac-toe/in-game.vue
@@ -4,8 +4,72 @@ definePageMeta({
 })
 
 useHead({ title: 'In Game' })
+
+const store = useGameTicTacToeStore()
+const { isPlayingVsAI, isCurrentPlayerX, isCurrentPlayerO, currentTurn, roundResults } = storeToRefs(store)
+
+const footerInfos = computed(() => {
+  const { X: xWins, O: oWins, ties } = roundResults.value
+  const xIdentity = isCurrentPlayerX.value ? ' (You)' : isPlayingVsAI.value ? ' (AI)' : '(Player 1)'
+  const oIdentity = isCurrentPlayerO.value ? ' (You)' : isPlayingVsAI.value ? ' (AI)' : '(Player 2)'
+
+  return [
+    {
+      label: `X ${xIdentity}`,
+      value: xWins,
+      isCurrentTurn: currentTurn.value === 'X',
+      bgClass: 'bg-[var(--color-primary)]'
+    },
+    { label: 'Ties', value: ties, bgClass: 'bg-[var(--color-neutral)]' },
+    {
+      label: `O ${oIdentity}`,
+      value: oWins,
+      isCurrentTurn: currentTurn.value === 'O',
+      bgClass: 'bg-[var(--color-secondary)]'
+    }
+  ]
+})
 </script>
 
 <template>
-  <div class="">Hello from playing</div>
+  <div class="flex max-h-screen w-screen flex-col items-center justify-center">
+    <h1 class="sr-only">Tic-Tac-Toe - In Game Screen</h1>
+
+    <div class="flex w-full max-w-[30rem] items-center justify-between gap-3 p-4">
+      <NuxtLink to="/games/tic-tac-toe">
+        <GamesTicTacToeIconGameLogo />
+      </NuxtLink>
+
+      <GamesTicTacToeShadow3dBox
+        interactive
+        :brightness="0.7"
+        small
+        class="aspect-square rounded-lg bg-[var(--color-neutral)] p-2"
+      >
+        <Icon
+          name="material-symbols:settings-heart-outline-rounded"
+          class="block h-6 w-6 min-w-6 text-[var(--color-text)]"
+        />
+      </GamesTicTacToeShadow3dBox>
+    </div>
+
+    <LazyGamesTicTacToeBoard />
+
+    <div class="grid w-full max-w-[30rem] grid-cols-3 place-items-center gap-4 p-4 text-[var(--color-text)]">
+      <div
+        v-for="info in footerInfos"
+        :key="info.label"
+        class="flex w-full flex-col rounded-xl p-3"
+        :class="[
+          {
+            'is-current-turn': info.isCurrentTurn
+          },
+          info.bgClass
+        ]"
+      >
+        <span class="text-sm uppercase">{{ info.label }}</span>
+        <span class="text-2xl font-bold">{{ info.value }}</span>
+      </div>
+    </div>
+  </div>
 </template>
